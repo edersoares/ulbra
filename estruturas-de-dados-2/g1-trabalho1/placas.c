45267350c46ef34e98bc8c74305159273966b353
Eder Soares <edersoares@me.com>
2016-09-06 19:51:59 -0300

    Criada funções de alocação dinâmica

--- estruturas-de-dados-2/g1-trabalho1/placas.c
+++ estruturas-de-dados-2/g1-trabalho1/placas.c
@@ -1,20 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct placa {
-    char letras[3];
+    char letras[4];
     int numeros;
 } Placa;
 
 typedef struct no {
-    struct placa placa;
+    struct placa *placa;
     struct no *esquerda;
     struct no *direita;
 } No;
 
+Placa * allocPlaca() {
+    return (Placa *) malloc(sizeof(Placa));
+}
+
+No * allocNo() {
+    return (No *) malloc(sizeof(No));
+}
+
 int main(int argc, char const *argv[]) {
 
-    printf("Hello!\n");
+    Placa *placa;
+    No *no;
+
+    placa = allocPlaca();
+    no = allocNo();
+
+    strcpy(placa->letras, "IUY");
+    placa->numeros = 1230;
+
+    no->placa = placa;
+
+    printf("Placa: %s%i\n", no->placa->letras, no->placa->numeros);
 
     return 0;
 }
